03c842bc6db4e6110a78253d7b9fa349f307557e
Maga Infox <magainfox@gmail.com>
2023-05-16 16:38:08 -0300

    add Rest API

--- js/index.js
+++ js/index.js
@@ -29,10 +29,58 @@ document.querySelector("footer").innerHTML = miFooter;
 //Index Ends
 
 //Rest API Starts
+let lon;
+let lat;
+let temperature = document.querySelector(".temp")
+let summary = document.querySelector(".summary")
+let loc = document.querySelector(".location")
+let icon = document.querySelector(".icon")
+const kelvin = 273.15
 
 
 
 
+
+
+window.addEventListener("load", () => {
+
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            //ID API
+            const api_id = "eb0327b3ea2a8aa6bd42a49e84badf54";
+
+            const url_base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${api_id}`;
+
+            fetch(url_base)
+                .then((response) => {
+                    console.log("RESPUESTA JSON");
+                    return response.json();
+                })
+
+                .then((data) => {
+                    console.log("ESTA ES LA DATA")
+                    console.log(data);
+
+                    temperature.textContent =
+                        Math.floor(data.main.temp - kelvin) + "ºC";
+                    summary.textContent = data.weather[0].description;
+                    loc.textContent = data.name + "," + " " + data.sys.country;
+                });
+
+        });
+
+
+    }
+
+
+});
 //Rest API Ends
 
 //Validación Starts
